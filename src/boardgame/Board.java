561d1cec07237e0e9a217a050a6ea0b1819d38b1
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-06-04 18:39:27 -0300

    starting to implement board and piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+import javafx.scene.input.PickResult;
+
+public class Board {
+    private int rows;
+    private int columns;
+    private Piece [][] pieces;
+
+    public Board(int columns, int rows) {
+        this.columns = columns;
+        this.rows = rows;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+}
