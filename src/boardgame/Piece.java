456043ebc8c9c6d00081f96e1188c78e36d1fc18
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-06-06 16:40:11 -0300

    possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,8 @@
 package boardgame;
 
-public class Piece {
+import javafx.geometry.Pos;
+
+public abstract class Piece {
     protected Position position;
     private Board board;
 
@@ -10,5 +12,25 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+
+    //this method calls the implemented class own possibleMoves method;
+    public boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] mat = possibleMoves();
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat.length; j++){
+                if(mat[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
