4dc07d95e337a53a51f5a556a02b356e0ba5aac8
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-06-06 16:09:59 -0300

    some adjusts in printing board

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,7 +28,7 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces){
         for(int i = 0; i < pieces.length; i ++){
-            System.out.print((8 - i ) + " ");
+            System.out.print((8 - i ) + "");
             for(int j = 0; j < pieces.length; j++){
                 printPiece(pieces[i][j]);
             }
@@ -39,14 +39,14 @@ public class UI {
 
     private static void printPiece(ChessPiece piece){
         if (piece == null) {
-            System.out.print("-");
+            System.out.print(" -");
         }
         else {
             if (piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+                System.out.print(" " + ANSI_WHITE + piece + ANSI_RESET);
             }
             else {
-                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+                System.out.print(" " + ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
     }
